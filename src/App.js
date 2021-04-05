019e9302e864ec9e7988e41ad26a65cb73cf91ac
Mitchell Hartigan <mitch.hartigan@gmail.com>
2021-04-05 12:13:26 -0700

    feat: configured global styles

--- src/App.js
+++ src/App.js
@@ -1,5 +1,26 @@
-function App() {
-  return <div></div>;
+import React from "react";
+import { ThemeProvider } from "styled-components";
+import "./index.css";
+
+import Homepage from "./pages/Homepage";
+
+export default function App() {
+  return (
+    <ThemeProvider theme={globalStyles}>
+      <Homepage />
+    </ThemeProvider>
+  );
 }
 
-export default App;
+const globalStyles = {
+  fontFamily: "",
+  fontSizeLg: "",
+  fontSizeMd: "",
+  fontSizeSm: "",
+  colors: {
+    mainBlue: "#007fff",
+    darkBlue: "#17224d",
+    brightBlue: "#17e9e1",
+    paleBlue: "#f5fafa",
+  },
+};
