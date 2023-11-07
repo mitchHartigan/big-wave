d77e60cb9bbb2d05ebdb231ebeecff3e5c8aeccc
Mitch Hartigan <42709932+mitchHartigan@users.noreply.github.com>
2023-11-07 15:27:37 -0800

    content: update brochures

--- src/pages/Tidal Wave Mixer/_technicalSpecs/_TechnicalSpecs.js
+++ src/pages/Tidal Wave Mixer/_technicalSpecs/_TechnicalSpecs.js
@@ -37,32 +37,11 @@ export class TechnicalSpecs extends React.Component {
                 Technical Specifications
               </Title>
             </VisibilitySensor>
-            <Spec name="Control Center Power:">
-              120 VAC / 1Ph / 60Hz, 20 amp circuit
-            </Spec>
-            <Spec name="Motor Type:">
-              NSF 61, 1.5HP, 230 VAC, water-filled, water-lubricated
-            </Spec>
-            <Spec name="RPM:">3100</Spec>
-            <Spec name="Power Draw:">14 amps, 1800 watts</Spec>
+            <Spec name="RPM:">3450</Spec>
             <Spec name="Footprint Diameter:">23" (58.42 cm)</Spec>
-            <Spec name="Height:">30.75” (78.10 cm)</Spec>
-
-            <Spec name="Mixer Assembly Weight:">35 lbs. (15.87 kg)</Spec>
-            <Spec name="Control Center Dimensions:">
-              40 lbs. (18.14 kg) (20” x 16” x 6” / 50.8 cm x 40.64 cm x 15.24
-              cm)
-            </Spec>
-            <Spec name="Control Center Material:">
-              Powder-coated steel, Type 3R enclosure
-            </Spec>
+            <Spec name="Height:">30.75” (78.10 cm) + 10" (25.4cm) handle</Spec>
+            <Spec name="Weight:">35 lbs. (15.87 kg)</Spec>
             <Spec name="Stand Material:">HDPE</Spec>
-            <Spec name="Motor Seals Material:">
-              Chlorine/chloramine-resistant NBR rubber
-            </Spec>
-            <Spec name="Wiring:">
-              NSF 61 & UL-listed submersible pump cable
-            </Spec>
             <DownloadButton href="spec_TidalWaveMixer.docx" target="_blank">
               Download the full Tidal Wave Mixer Specifications (.docx).
             </DownloadButton>
